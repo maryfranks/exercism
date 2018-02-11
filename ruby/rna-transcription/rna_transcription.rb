05be8f7c22b1e89e7895c5cc826417a01811fecc
Mary Franks <maryfranks@gmail.com>
2018-02-11 09:55:52 -0500

    update of_dna to handle strings longer than 1 letter

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,17 +1,27 @@
+require 'pry'
+
 class Complement
 
   def self.of_dna (nucleotide)
 
-    case nucleotide
-    when 'C'
-      return 'G'
-    when 'G'
-      return 'C'
-    when 'T'
-      return 'A'
-    when 'A'
-      return 'U'
+    rna = []
+
+    nucleotide.split(//).each do | letter |
+
+      case letter
+      when 'C'
+        rna.push 'G'
+      when 'G'
+        rna.push 'C'
+      when 'T'
+        rna.push 'A'
+      when 'A'
+        rna.push 'U'
+      end
+
     end
+
+    return rna.join
 
   end
 
--- ruby/rna-transcription/rna_transcription_test.rb
+++ ruby/rna-transcription/rna_transcription_test.rb
@@ -24,7 +24,7 @@ class RnaTranscriptionTest < Minitest::Test
   end
 
   def test_rna_complement
-    skip
+    # skip
     assert_equal 'UGCACCAGAAUU', Complement.of_dna('ACGTGGTCTTAA')
   end
 
