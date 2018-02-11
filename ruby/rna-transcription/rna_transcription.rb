749641c0f67b7a9af530ea8eef521fc0803d9a4d
Mary Franks <maryfranks@gmail.com>
2018-02-11 09:14:23 -0500

    convert individual nucleotides to rna

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,3 +1,18 @@
 class Complement
 
+  def self.of_dna (nucleotide)
+
+    case nucleotide
+    when 'C'
+      return 'G'
+    when 'G'
+      return 'C'
+    when 'T'
+      return 'A'
+    when 'A'
+      return 'U'
+    end
+
+  end
+
 end
--- ruby/rna-transcription/rna_transcription_test.rb
+++ ruby/rna-transcription/rna_transcription_test.rb
@@ -9,17 +9,17 @@ class RnaTranscriptionTest < Minitest::Test
   end
 
   def test_rna_complement_of_guanine_is_cytosine
-    skip
+    # skip
     assert_equal 'C', Complement.of_dna('G')
   end
 
   def test_rna_complement_of_thymine_is_adenine
-    skip
+    # skip
     assert_equal 'A', Complement.of_dna('T')
   end
 
   def test_rna_complement_of_adenine_is_uracil
-    skip
+    # skip
     assert_equal 'U', Complement.of_dna('A')
   end
 
