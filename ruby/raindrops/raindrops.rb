9b3d88d8369e9e5afdbb37952346c2cf76aedb6b
Mary Franks <maryfranks@gmail.com>
2018-02-12 07:34:16 -0500

    add cases for number divisible by 3, 5, and 7

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,7 +2,15 @@ class Raindrops
 
   def self.convert (number)
 
-    return number.to_s  
+    if number % 3 == 0
+      return "Pling"
+    elsif number % 5 == 0
+      return "Plang"
+    elsif number % 7 == 0
+      return "Plong"
+    else
+      return number.to_s
+    end
 
   end
 
