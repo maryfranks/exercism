0003cb94075a071b363b2f6c555d910bddcbd482
Mary Franks <maryfranks@gmail.com>
2018-07-16 09:15:32 -0400

    refactor to assign value to name if name == null

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,11 +1,9 @@
 class Twofer {
     String twofer(String name) {
-      if (name != null) {
-        String output = String.format("One for %s, one for me.", name);
-        return output;
-    } else {
-        return "One for you, one for me.";
-    }
-
-    }
+      if (name == null) {
+        name = "you";
+      }
+      String output = String.format("One for %s, one for me.", name);
+      return output;
+      }
 }
