4197791983dd97bdc07e3689143295e0dc16fc88
Mary Franks <maryfranks@gmail.com>
2018-07-18 14:21:19 -0400

    remove comments

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,16 +1,11 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        // if (inputString.equals("")) {
-        //   return inputString;
-        // }
-        // else {
           StringBuilder reversedString = new StringBuilder();
           reversedString.append(inputString);
           reversedString = reversedString.reverse();
 
           return reversedString.toString();
-        // }
     }
 
 }
