d730883f67944481531a20dc63ba6a084c24cf82
Mary Franks <maryfranks@gmail.com>
2018-07-19 09:47:03 -0400

    throws exception if strings are not equal in length

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -4,20 +4,24 @@ class Hamming {
     private String right;
 
     Hamming(String leftStrand, String rightStrand) {
+      if (leftStrand.length() != rightStrand.length()) {
+        throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+      }
       this.left = leftStrand;
       this.right = rightStrand;
     }
 
     int getHammingDistance() {
-      int distance = 0;
-      int i = 0;
-      while (i < left.length()) {
-        if (left.charAt(i) != right.charAt(i)) {
-          distance++;
+        int distance = 0;
+        int i = 0;
+        while (i < left.length()) {
+          if (left.charAt(i) != right.charAt(i)) {
+            distance++;
+          }
+          i++;
         }
-        i++;
+        return distance;
       }
-      return distance;
-    }
+
 
 }
