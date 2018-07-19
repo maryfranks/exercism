269009ed927bbff85ed77750a6140dc949b08444
Mary Franks <maryfranks@gmail.com>
2018-07-19 09:23:20 -0400

    counts distance on empty string

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,11 +1,23 @@
 class Hamming {
 
+    private String left;
+    private String right;
+
     Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      this.left = leftStrand;
+      this.right = rightStrand;
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      int distance = 0;
+      int i = 0;
+      while (i < left.length()) {
+        if (left.charAt(i) != right.charAt(i)) {
+          distance++;
+        }
+        i++;
+      }
+      return distance;
     }
 
 }
