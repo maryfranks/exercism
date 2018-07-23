d6909456c4b8daf426fbcc2aa445cf13dd30ee7f
Mary Franks <maryfranks@gmail.com>
2018-07-23 09:25:59 -0400

    return pling, plang, and plong if number is divisible by 3, 5, or 7

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,18 @@
 class RaindropConverter {
 
     String convert(int number) {
+      if (number % 3 == 0) {
+        return "Pling";
+      }
+      else if (number % 5 == 0) {
+        return "Plang";
+      }
+      else if (number % 7 == 0) {
+        return "Plong";
+      }
+      else {
         return Integer.toString(number);
+      }
     }
 
 }
