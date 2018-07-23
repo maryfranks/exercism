487f37c30e6bc6e1f3fbd50ed20c96aadc931319
Mary Franks <maryfranks@gmail.com>
2018-07-23 09:35:06 -0400

    return PlingPlangPlong if number is divisible by 7, 5, and 3

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,10 @@
 class RaindropConverter {
 
     String convert(int number) {
-      if (number % 5 == 0 & number % 3 == 0) {
+      if (number % 7 == 0 & number % 5 == 0 & number % 3 == 0) {
+        return "PlingPlangPlong";
+      }
+      else if (number % 5 == 0 & number % 3 == 0) {
         return "PlingPlang";
       }
       else if (number % 7 == 0 & number % 3 == 0) {
