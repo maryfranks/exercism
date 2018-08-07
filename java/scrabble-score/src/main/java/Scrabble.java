4ed6664e7f30e470b4a825bcdd8311de90d51eb5
Mary Franks <maryfranks@gmail.com>
2018-08-07 08:22:32 -0400

    add array with characters of different points

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,11 +1,23 @@
 class Scrabble {
 
+  public static final char[] ONE_POINT_ARRAY = {"a", "w", "i", "o", "u", "l", "n", "r", "s", "t"};
+  public static final char[] TWO_POINT_ARRAY = {"d", "g"};
+  public static final char[] THREE_POINT_ARRAY = {"b", "c", "m", "p"};
+  public static final char[] FOUR_POINT_ARRAY = {"f", "h", "v", "w", "y"};
+  public static final char[] FIVE_POINT_ARRAY = {"k"};
+  public static final char[] EIGHT_POINT_ARRAY = {"j", "x"};
+  public static final char[] TEN_POINT_ARRAY = {"q", "z"};
+
+  private static String word;
+
     Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      this.word = word;
     }
 
     int getScore() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      array = word.toCharArray();
+      array
+      return 1;
     }
 
 }
